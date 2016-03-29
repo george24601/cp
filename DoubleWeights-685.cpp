26b3544cc1fe1fef306f493b6f4d17ab8c5fb62f
George Li <george.li.24601@gmail.com>
2016-03-28 23:08:30 -0400

    SRM 686 Div2 level 2

--- DoubleWeights-685.cpp
+++ DoubleWeights-685.cpp
@@ -36,43 +36,48 @@ typedef vector<vector<int> > SAL;
  ways(index + 1, num) = ways(index, n1) n1 < num  or n1 % num != 0
  check all on ways(N, n), add them all
  */
-class DoubleWeights{
-	int tw [21][21][180];
+class DoubleWeights {
+	int tw[21][21][180];
 	int n;
 
-
-public :
-	int minimalCost(vector<string> weight1, vector<string> weight2){
+public:
+	int minimalCost(vector<string> weight1, vector<string> weight2) {
 		n = weight1.size();
 
-		int minW = INF;
-
-		LP(i, 0, n){
-			LP(j, 0, n){
+		LP(i, 0, n)
+		{
+			LP(j, 0, n)
+			{
 				memset(tw[i][j], -1, sizeof(tw[i][j]));
 			}
 		}
 
 		tw[0][0][0] = 0;
 
-		LPE(pathLen, 1, n){
-			LP(from, 0, n){
-				LP(to, 0, n){
-					if (weight1[from][to] == '.')
-						continue; //no path
+		LPE(pathLen, 1, n)
+		{
+			LP(from, 0, n)
+			{
+				LP(fromW1, 0, n * 9)
+				{
+					if (tw[pathLen - 1][from][fromW1] < 0)
+						continue;
 
-					int el1 = weight1[from][to]  - '0';
-					int el2 = weight2[from][to]  - '0';
+					tw[pathLen][from][fromW1]  = min(tw[pathLen][from][fromW1], tw[pathLen - 1][from][fromW1]);
 
-					LP(fromW1, 0, n * 9){
-						if (tw[pathLen - 1][from][fromW1] < 0)
-							continue;
+					LP(to, 0, n)
+					{
+						if (weight1[from][to] == '.')
+							continue; //no path
+
+						int el2 = weight2[from][to] - '0';
 
 						int newToW2 = tw[pathLen - 1][from][fromW1] + el2;
 
+						int el1 = weight1[from][to] - '0';
 						int oldToW2 = tw[pathLen - 1][to][fromW1 + el1];
 
-						if(oldToW2 < 0 || oldToW2 > newToW2)
+						if (oldToW2 < 0 || oldToW2 > newToW2)
 							tw[pathLen][to][fromW1 + el1] = newToW2;
 						else
 							tw[pathLen][to][fromW1 + el1] = oldToW2;
@@ -81,8 +86,10 @@ public :
 			}
 		}
 
+		int minW = INF;
 
-		LPE(w1, 1, (n -1) * 9){
+		LPE(w1, 1, (n -1) * 9)
+		{
 			if (tw[n][1][w1] <= 0)
 				continue;
 
@@ -93,6 +100,3 @@ public :
 	}
 };
 
-int main() {
-	return 0;
-}
