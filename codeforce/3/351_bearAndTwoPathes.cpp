bf1b43282b8c07474b3ca9befc6b15e173dadfe3
George Li <george.li.24601@gmail.com>
2016-05-14 15:36:59 -0400

    fixed 351 problem d

--- codeforce/3/351_bearAndTwoPathes.cpp
+++ codeforce/3/351_bearAndTwoPathes.cpp
@@ -28,44 +28,15 @@ typedef vector<vector<int> > SAL;
 #define Ep 1e-9
 
 /*
-consider the sequence a, c, b, d, all distinct
-swap between a and 1
-c and 2,
-d and n-3
-b and n;
+ first path
+ a -> c->p1....p2-> d -> b
 
-add a map, add both theory to actual alias
+ second path
+ c->a ->p1...p2->b ->d
 
-first path
-1...n, print alias[num]
+ so n+1 additional edges needed
 
-second path
-
-2 .... n-4 -> 1 -> n-1 -> n -> n-2 -> n-3
-
-so alias
-1 -> a
-n -> b
-2 -> c
-n-3 -> d
-a -> 1
-b -> n
-c -> 2
-d -> n - 3
-
-others same
-
-a-> c...p->d->q->b
-
-c..p->a->q -> b -> d
-
-set of unused ids;
-
-get set of unused ids;
-
-take one for p, one for q
-
-remaining will be the one between c and p, exclusive
+ print p1 to p2
 
 
  */
@@ -74,7 +45,6 @@ int n, k, a, b, c, d;
 int const MaxSize = 5001;
 
 vector<int> unused;
-int p, q;
 
 void printP(vector<int>& p){
 	printf("%d", p[0]);
@@ -86,46 +56,41 @@ void printP(vector<int>& p){
 }
 
 void ab(){
-		//a-> c...p->d->q->b
+		//a-> c->p...q->d->b
 	vector<int> ab;
 	ab.push_back(a);
 	ab.push_back(c);
 
 	int unusedSize = unused.size();
 
-	LP(i, 2, unusedSize)
+	LP(i, 0, unusedSize)
 		ab.push_back(unused[i]);
 
-	ab.push_back(p);
 	ab.push_back(d);
-	ab.push_back(q);
 	ab.push_back(b);
 
 	printP(ab);
 }
 
+//c->a->p...q->b->d
 void cd(){
-//	c..p->a->q -> b -> d
 
 	int unusedSize = unused.size();
 	vector<int> cd;
 	cd.push_back(c);
+	cd.push_back(a);
 
-	LP(i, 2, unusedSize)
+	LP(i, 0, unusedSize)
 		cd.push_back(unused[i]);
 
-	cd.push_back(p);
-	cd.push_back(a);
-	cd.push_back(q);
 	cd.push_back(b);
 	cd.push_back(d);
-
 
 	printP(cd);
 }
 
 int main() {
-	freopen("/Users/georgeli/B_1.in", "r", stdin);
+	//freopen("/Users/georgeli/A_1.in", "r", stdin);
 	//freopen("/Users/georgeli/Downloads/B-small-attempt1.in", "r", stdin);
 	//freopen("/Users/georgeli/B_1.out", "w", stdout);
 
@@ -133,20 +98,17 @@ int main() {
 
 	scanf("%d %d %d %d", &a, &b, &c, &d);
 
-	if(k < (n - 1) + 3 || n < 6){
+	if(k < (n -1)  + 2 || n < 5){
 		printf("-1\n");
 		return 0;
 	}
 
-
 	unused.clear();
+
 	LPE(i, 1, n){
 		if(i != a && i != b && i != c && i != d)
 			unused.push_back(i);
 	}
-
-	p = unused[0];
-	q = unused[1];
 
 	ab();
 	cd();
