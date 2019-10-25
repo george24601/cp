6364802d24deac970ff3764b8507f02fd67cda98
George Li <george.li.24601@gmail.com>
2019-10-25 17:46:37 -0400

    py script

---
+++ lib/bits.cpp
@@ -0,0 +1,4 @@
+* swap two numbers with just `^`
+* Find the unique number with all other dups, with just `^`
+* implement pow with bit ops and `*`
+* find the lowerest 2^n LT current number ```n | = n >> 1; n |= n >> 2; n |= n >> 4;....; n |= n >> 32```
---
+++ misc.md
@@ -0,0 +1,6 @@
+### Double type precision
+consider double binary representation, sign + expoennt + manitissa, this defines that
+5.2 => 101.0011011.... => 1.010011011 * 2^2
+
+2^2 becomes 2+127 = 129
+1.010011011 becomes 010011011 in manitissa
