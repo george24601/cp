9ecc636244df64e4be72495d85da3fc580825159
George Li <george.li@instacart.com>
2024-05-12 13:45:41 -0400

    wip

--- go_ws/shasta/model.go
+++ go_ws/shasta/model.go
@@ -1,5 +1,15 @@
 package shasta
 
+// TableInfo provides meta data describing a DB table.
+type TableInfo struct {
+	ID      int64  `json:"id"`
+	Name    string `json:"name"`
+	Charset string `json:"charset"`
+	Collate string `json:"collate"`
+	// Columns are listed in the order in which they appear in the schema.
+	Columns []*ColumnInfo `json:"cols"`
+}
+
 // ColumnInfo provides meta data describing of a table column.
 type ColumnInfo struct {
 	ID                  int64               `json:"id"`
@@ -10,7 +20,4 @@ type ColumnInfo struct {
 	GeneratedExprString string              `json:"generated_expr_string"`
 	GeneratedStored     bool                `json:"generated_stored"`
 	Dependencies        map[string]struct{} `json:"dependencies"`
-	types.FieldType     `json:"type"`
-	//	State               SchemaState `json:"state"`
-	Comment string `json:"comment"`
 }
